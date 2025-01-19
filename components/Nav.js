7d3c6286c8790a911e0ce955ff627e1e451b0cb6
Antonio Ortega <antonioortega@Mac.hsd1.or.comcast.net>
2025-01-18 18:53:17 -0800

    fix link for linkedin and rm unsued var

--- components/Nav.js
+++ components/Nav.js
@@ -8,22 +8,12 @@ import {
 } from '@fortawesome/free-brands-svg-icons'
 import SocialLink from './SocialLink'
 
-const links = [
-  { href: 'https://github.com/antonioortegajr', label: 'GitHub', num: 0 },
-  {href: 'https://twitter.com/antonioortegajr', label: 'twitter', num: 1 },
-  { href: 'https://facebook.com/antonioortegajr', label: 'facebook', num: 2 },
-  { href: 'https://www.linkedin.com/in/antonioortegajr/', label: 'linkedin', num: 3 },
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
-
 const iconsArray = [faGithub, faTwitter, faFacebook, faLinkedin]
 const Nav = () => (
   <nav>
     <ul>
     <SocialLink link='https://github.com/antonioortegajr' icon={faGithub} />
-    <SocialLink link='https://twitter.com/antonioortegajr' icon={faLinkedin} />
+    <SocialLink link='https://www.linkedin.com/in/antonioortegajr/' icon={faLinkedin} />
     <SocialLink link='https://facebook.com/antonioortegajr'icon={faFacebook} />
     <SocialLink link='https://twitter.com/antonioortegajr' icon={faTwitter} />
 
