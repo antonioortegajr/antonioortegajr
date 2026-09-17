a00c5c512c336cdc1ecb0497d87ff5c9ada1e54e
Antonio Ortega <antonioortegajr@gmail.com>
2026-09-16 21:06:09 -0700

    Add SadBunny import to general.tests.js
    Agent: pi5-agent on pi5-home (qwen3.5:4b-q4_K_M)

--- tests/__tests__/general.tests.js
+++ tests/__tests__/general.tests.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Home from '../../pages/index';
+import SadBunny from '../../components/SadBunny';
 
 // these tests are all AI generated
 
